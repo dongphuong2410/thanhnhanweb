3729f8ed20ce78bf494ad47447bfb6ca854276a8
Phuong Do <dongphuong2004@gmail.com>
2016-06-03 15:06:05 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+thanhnhan
+=========
+
+A Symfony project created on June 3, 2016, 2:04 pm.
---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/")
+     *
+     * @return array
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+}
